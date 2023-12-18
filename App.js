72aefe31f43248ea024b839a14ecb9527465b951
Khasanboy Akhmedov <paragon7400@gmail.com>
2023-12-18 23:25:32 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>This is my first app in react native</Text>
+      </View>
+      <Text>Helo world</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
